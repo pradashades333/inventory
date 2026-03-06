48f0f39a5b6aa81502406c982f9ecb88d4934b36
pradashades333 <alexdreve03@gmail.com>
2026-03-06 23:35:44 +0100

    added controller for arcs

---
+++ controllers/arcsController.js
@@ -0,0 +1,8 @@
+const pool = require("../db/pool");
+
+module.exports = {
+  index: async (req, res) => {
+    const { rows } = await pool.query("SELECT * FROM arc");
+    res.render("arcs/index", { arcs: rows });
+  }
+};
