9e1b59c137d92e8b0ac72c6442b7135f1097b721
pradashades333 <alexdreve03@gmail.com>
2026-03-08 18:01:03 +0100

    added show query

--- controllers/arcsController.js
+++ controllers/arcsController.js
@@ -5,6 +5,11 @@ module.exports = {
     const { rows } = await pool.query("SELECT * FROM arc");
     res.render("arcs/index", { arcs: rows });
   }
+  ,
 
-  
+  show: async (req,res) => {
+    const { rows } = await pool.query("SELECT * FROM arc WHERE id = $1", [req.params.id]);
+    res.render("arcs/show", { arc: rows[0] });
+  }
+
 };
