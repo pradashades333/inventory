f54467deaa52618fcc087db270cd0fab48b2d828
pradashades333 <alexdreve03@gmail.com>
2026-03-08 19:02:20 +0100

    added create and new arc methods

--- controllers/arcsController.js
+++ controllers/arcsController.js
@@ -4,12 +4,20 @@ module.exports = {
   index: async (req, res) => {
     const { rows } = await pool.query("SELECT * FROM arc");
     res.render("arcs/index", { arcs: rows });
-  }
-  ,
+  },
 
   show: async (req,res) => {
     const { rows } = await pool.query("SELECT * FROM arc WHERE id = $1", [req.params.id]);
     res.render("arcs/show", { arc: rows[0] });
+  },
+
+  newArc:async (req, res) => {
+    res.render("arcs/form")
+  },
+
+  create: async (req,res) => {
+    const { rows } = await pool.query("INSERT INTO arc (name,description) VALUES ($1,$2)", [req.body.name, req.body.description]);
+    res.redirect("/arcs")
   }
 
 };
