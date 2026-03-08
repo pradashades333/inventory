323c3bd52dba9636d3deb84843a4cbe5f1256dab
pradashades333 <alexdreve03@gmail.com>
2026-03-08 19:31:58 +0100

    added all functions

--- controllers/arcsController.js
+++ controllers/arcsController.js
@@ -18,6 +18,21 @@ module.exports = {
   create: async (req,res) => {
     const { rows } = await pool.query("INSERT INTO arc (name,description) VALUES ($1,$2)", [req.body.name, req.body.description]);
     res.redirect("/arcs")
+  },
+  
+  edit:async (req,res) => {
+    const { rows } = await pool.query("SELECT * FROM arc WHERE id = $1", [req.params.id]);
+    res.render("arcs/form", { arc: rows[0] })
+  },
+
+  update: async (req, res) => {
+    await pool.query("UPDATE arc SET name = $1, description = $2 WHERE id = $3", [req.body.name, req.body.description, req.params.id]);
+    res.redirect("/arcs");
+  },
+
+  destroy:async (req,res) => {
+    await pool.query("DELETE FROM arc WHERE id = $1", [req.params.id]);
+    res.redirect("/arcs")
   }
 
 };
